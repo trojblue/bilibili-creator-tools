e09b34069bc9d6f0d50aeac2c11ebb4484ac36d8
trojblue <trojblue@gmail.com>
2019-04-18 17:10:40 -0400

    ==

---
+++ meme_generator.py
@@ -0,0 +1,21 @@
+import random
+from typing import List, Dict
+
+"""
+TO-Do:
+1. finish simple meme generator(txt based, no tag)      -- IN PROCESS
+2. finish structure design for complex meme generator
+3. implement complex meme generator
+4. link complex meme gen to MAIN
+5. write GUI for it
+"""
+
+class SimpleMemeGenerator():
+    def __init__(self, file_name:str) -> None:
+        pass
+
+    def generate(self):
+        pass
+
+
+
